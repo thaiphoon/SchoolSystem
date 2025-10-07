fc8d5485be27994a84e3f0432eb74018613d01a0
AnthonyRG <anthonyrizk89@gmail.com>
2025-10-07 16:24:20 +0200

    Added data structures

--- src/system/data/grade/Grade.java
+++ src/system/data/grade/Grade.java
@@ -1,4 +1,25 @@
 package system.data.grade;
 
 public abstract class Grade {
+    int courseId;
+    int gradeId;
+    String value;
+
+    public Grade(int gradeID, int courseId, String value) {
+        this.gradeId = gradeID;
+        this.courseId = courseId;
+        this.value = value;
+    }
+
+    public int getGradeId() {
+        return gradeId;
+    }
+
+    public int getCourseId() {
+        return courseId;
+    }
+
+    public String getValue() {
+        return value;
+    }
 }
--- src/system/data/person/Teacher.java
+++ src/system/data/person/Teacher.java
@@ -1,4 +1,10 @@
 package system.data.person;
 
 public class Teacher extends Person{
+
+    public Teacher(int personId, String name){
+        super(personId, name);
+    }
+
+
 }
