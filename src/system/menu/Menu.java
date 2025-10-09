6b95c1ab7e0f6fae40e83ac993c86a990e5ac20a
AnthonyRG <anthonyrizk89@gmail.com>
2025-10-09 11:49:55 +0200

    Added menu for teacher

--- src/system/menu/Menu.java
+++ src/system/menu/Menu.java
@@ -10,6 +10,7 @@ import system.menu.teacher.MTeacher;
 import system.inputHandling.SingleIntegerInput;
 import system.menu.teacher.MTeacher;
 import system.inputHandling.SingleIntegerInput;
+import system.menu.teacher.MTeacher;
 
 import java.io.BufferedReader;
 import java.io.IOException;
