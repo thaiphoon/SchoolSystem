03ca5ef65279a5d34783f1613eed139b5c72d1a4
dkwa0000 <joakim.tubring@gmail.com>
2025-10-09 15:22:02 +0200

    Adding the pending grade to Json parser

--- src/system/data/grade/PendingGrade.java
+++ src/system/data/grade/PendingGrade.java
@@ -1,7 +1,9 @@
 package system.data.grade;
 
 public class PendingGrade extends Grade{
-    public PendingGrade(int gradeId, int courseId, String value) {
+    public PendingGrade(int gradeId, int courseId, String value)
+    {
         super(gradeId, courseId, value);
+        this.type = "pendinggrade";
     }
 }
